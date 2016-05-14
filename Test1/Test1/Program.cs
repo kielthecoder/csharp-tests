42bbd273527de255e3457cab48f4eda2fa2df1fe
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2016-05-13 20:14:48 -0400

    First test with arrays from Getting Started with C#

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pins = { 9, 3, 7, 2 };
+
+            foreach (int pin in pins)
+            {
+                Console.WriteLine(pin);
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
